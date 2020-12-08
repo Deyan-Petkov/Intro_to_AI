406a8601afbe8ee58a29b052f223f4dc4461f194
Deyan-Petkov <deyan.post@gmail.com>
2020-12-08 10:21:38 +0000

    added function for testing different kernels with different values of C and applying it to the dataset being standardized fully, partly or not standardized at all

--- Deyan Petkov/SVM.py
+++ Deyan Petkov/SVM.py
@@ -70,6 +70,7 @@ df = df[df.VisitorType != 'Other']
 leVisitor = LabelEncoder()
 #convert VisitorType from categorical to numerical
 df['VisitorType'] = leVisitor.fit_transform(df['VisitorType'])
+
 leVisitor_mapping = dict(zip(leVisitor.classes_ ,
                                  leVisitor.transform(leVisitor.classes_)))
 
@@ -87,7 +88,6 @@ for x in df.columns:
         
 X = df[result].values
 y = df['Revenue'].values
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42) 
 
 
@@ -265,15 +265,51 @@ df.boxplot(['Administrative', 'Administrative_Duration', 'Informational', 'Infor
        'ProductRelated', 'BounceRates', 'ExitRates', 'PageValues'], notch = True);
 
 
+    
+    
+'''======================================================'''
+
+"""We continue with Support Verctor Regression
+   still having columns  'OperatingSystems', 'Browser' and 'TrafficType'
+   dropped from the dataset. """
 
 
 from sklearn.preprocessing import StandardScaler
 from  sklearn.svm import SVR
 from sklearn.metrics import mean_squared_error
 
-"""We continue with Support Verctor Regression
-   still having columns  'OperatingSystems', 'Browser' and 'TrafficType'
-   dropped from the dataset. """
+
+
+#Regression without andy standardization
+
+
+#c_values = [0.01, 0.1, 1.0, 10, 100]
+c_values = [0.01, 0.1, 1.0, 10, 100]
+
+
+def SVRfit_pred(fitX, fitY, testSet, c_values, description):
+    msqe = []    
+    for x in ['rbf', 'linear', 'poly', 'sigmoid']:
+        print(x)
+        for y in c_values:
+            print(y)
+            svr = SVR(kernel= x, C = y, gamma='auto')
+            #svr = SVR(kernel= x, C = 0.1 )
+            #svr = SVR(kernel= x, C = 100 )
+            svr.fit(fitX, fitY)
+            y_pred = svr.predict(testSet)
+            #calculate the squared mean error of the model prediction
+            msqe.append([x, y, np.sqrt(mean_squared_error(y_test, y_pred))])
+    
+    results = pd.DataFrame(msqe, columns =["Kernel","C_value", "MSQE"])
+    print (description + "  : \n", results)
+    
+
+        
+SVRfit_pred(X_train, y_train, X_test, c_values, "SVR results without any standardization")
+    
+'''============================================================'''
+
 
 #Standardizes the data making it easier to compare 
 # and reduces the deviation 
@@ -289,23 +325,108 @@ std_X_train = pd.DataFrame(stand_X_train)
 pd.DataFrame(stand_X_train).head(20)
 
 
-msqe = []    
 
-for x in ['rbf', 'linear','sigmoid', 'poly']:
-    svr = SVR(kernel= x )
-    #svr = SVR(kernel= x, C = 0.1 )
-    #svr = SVR(kernel= x, C = 100 )
-    svr.fit(stand_X_train, y_train)
-    y_pred = svr.predict(stand_X_test)
-    #calculate the squared mean error of the model prediction
-    msqe.append([x, np.sqrt(mean_squared_error(y_test, y_pred))])
+#show the datatypes of all columns
+df.dtypes
+
+SVRfit_pred(stand_X_train, y_train, stand_X_test, c_values, "SVR results X_train and X_test standardized ")
+
+'''======================================================'''
+
+'''' 
+print (y_train.reshape(-1, 1))
+stdYScaler = StandardScaler().fit (y_train.reshape(-1,1), y_test.reshape(-1, 1))
+stand_y_train = stdYScaler.transform(y_train.reshape(-1, 1))
+stand_y_test = stdYScaler.transform(y_test.reshape(-1, 1))
+
+SVRfit_pred(stand_X_train, y_train, stand_X_test, c_values, 
+'''
+
+
+standardized_df = df
+stdScaler = StandardScaler().fit(standardized_df)
+standardized_df = stdScaler.transform(standardized_df)
+standardized_df = pd.DataFrame(standardized_df)
+standardized_df.head(5)
+standardized_df.isnull().sum()
+
+X = standardized_df.drop(columns = [14]).values
+y = standardized_df[14].values
+X.shape
+y.shape
+
+pd.DataFrame(X).head(5)
+pd.DataFrame(y).head(5)
+standardized_df.head(5)
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42) 
+
+
+SVRfit_pred(X_train, y_train, X_test, c_values, "All data standardized")
+
+
+
+'''================================================================'''
+#Take only the columns which doesn't hold binary type of data(categorical) 
+#and standardize only them as standardizing a categorical features  will 
+#assign a distribution which is not desirable.
+
+df.describe()
+numDf = df[['Administrative', 'Administrative_Duration', 'Informational', 'Informational_Duration',
+       'ProductRelated', 'ProductRelated_Duration', 'BounceRates', 'ExitRates', 'PageValues' ]].copy()
+
+#Make dataframe with only categorical features
+categDf = df[['SpecialDay', 'Month', 'Region', 'VisitorType', 'Weekend', 'Revenue']].copy()
+
+numDf.head(5)
+categDf.head(5)
+#fit and transform the dataframe with non categorical data
+stdScaler = StandardScaler().fit(numDf)
+transformed_numDf = stdScaler.transform(numDf)
+#create a dataframe with the transformed features and see how it looks
+transformed_numDf = pd.DataFrame(transformed_numDf)
+transformed_numDf.describe()
+
+#check for null values before and after concatenating the standardized and the non standardized dataframes
+transformed_numDf.isnull().sum()
+#concatenate both dataframes (standardized and non standardized features )
+standardized_df = pd.concat([categDf, transformed_numDf], axis=1).reindex(categDf.index)
+#check again for null values (we have 129 rows)
+standardized_df.isnull().sum()
+#we drop this columns are they are not so many
+standardized_df.shape
+standardized_df.dropna(inplace=True)
+standardized_df.isnull().sum()
+
+#see how the dataframe looks after these manipulations
+standardized_df.shape
+standardized_df.head(5)
+standardized_df.describe()
+standardized_df.dtypes
+
+
+#split the dataframe into X and Y for training and testing
+result = []
+for x in standardized_df.columns:
+    if x != 'Revenue':
+        result.append(x)
+        
+
+X = standardized_df[result].values
+y = standardized_df['Revenue'].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42) 
+
+
+#train and test using the preset values for C in c_values and all the kernels
+SVRfit_pred(stand_X_train, y_train, stand_X_test, c_values, 
+            "SVR results only noncategorical data standardized")
     
-results = pd.DataFrame(msqe, columns =["Kernel", "MSQE"])
-print ("SVR results default : \n", results)
+
 
 
 """
-y not normalized
+y not standardized
 
 SVR results C = 0.1: 
      Kernel  MSQE
@@ -329,7 +450,7 @@ SVR results C = 100:
 3     poly  0.34
 ===================
 
-All data normalized: 
+All data standardized: 
 
 
 SVR results default:
@@ -338,6 +459,97 @@ SVR results default:
 1   linear   0.90
 2  sigmoid  91.45
 3     poly   0.94
+
+====================
+SVR results only noncategorical data standardized : 
+     Kernel   MSQE
+0      rbf   0.37
+1   linear   0.37
+2  sigmoid  95.17
+3     poly   0.38
+
+====================
+SVR results without any standardization  : 
+     Kernel   MSQE
+0      rbf   0.29
+1   linear   0.32
+2  sigmoid  90.86
+3     poly   0.33
+
 """
 
+"""
+about 3hr
+SVR results X_train and X_test standardized  : 
+      Kernel  C_value     MSQE
+0       rbf     0.01     0.30
+1       rbf     0.10     0.29
+2       rbf     1.00     0.29
+3       rbf    10.00     0.30
+4       rbf   100.00     0.35
+5    linear     0.01     0.32
+6    linear     0.10     0.32
+7    linear     1.00     0.32
+8    linear    10.00     0.32
+9    linear   100.00     0.32
+10     poly     0.01     0.33
+11     poly     0.10     0.34
+12     poly     1.00     0.33
+13     poly    10.00     0.35
+14     poly   100.00     0.37
+15  sigmoid     0.01     0.79
+16  sigmoid     0.10     9.04
+17  sigmoid     1.00    90.86
+18  sigmoid    10.00   908.32
+19  sigmoid   100.00  9207.58
 
+
+All data standardized  : 
+      Kernel  C_value     MSQE
+0       rbf     0.01     0.92
+1       rbf     0.10     0.82
+2       rbf     1.00     0.80
+3       rbf    10.00     0.80
+4       rbf   100.00     0.88
+5    linear     0.01     0.90
+6    linear     0.10     0.90
+7    linear     1.00     0.90
+8    linear    10.00     0.90
+9    linear   100.00     0.90
+10     poly     0.01     0.98
+11     poly     0.10     0.95
+12     poly     1.00     0.94
+13     poly    10.00     0.90
+14     poly   100.00     0.94
+15  sigmoid     0.01     1.14
+16  sigmoid     0.10     6.04
+17  sigmoid     1.00    91.45
+18  sigmoid    10.00   914.19
+19  sigmoid   100.00  8885.94
+
+
+SVR results only noncategorical data standardized  : 
+      Kernel  C_value     MSQE
+0       rbf     0.01     0.37
+1       rbf     0.10     0.37
+2       rbf     1.00     0.37
+3       rbf    10.00     0.38
+4       rbf   100.00     0.42
+5    linear     0.01     0.37
+6    linear     0.10     0.37
+7    linear     1.00     0.37
+8    linear    10.00     0.37
+9    linear   100.00     0.37
+10     poly     0.01     0.37
+11     poly     0.10     0.37
+12     poly     1.00     0.38
+13     poly    10.00     0.40
+14     poly   100.00     0.41
+15  sigmoid     0.01     0.88
+16  sigmoid     0.10     9.52
+17  sigmoid     1.00    95.17
+18  sigmoid    10.00   952.13
+19  sigmoid   100.00  9521.39
+
+
+"""
