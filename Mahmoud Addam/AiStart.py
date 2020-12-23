fd24a39d64bab19008bf37b2300bb609dadedd0f
swagmanfrenchie <mahmoud99a@gmail.com>
2020-12-23 15:41:20 +0000

    Update AiStart.py

--- Mahmoud Addam/AiStart.py
+++ Mahmoud Addam/AiStart.py
@@ -5,6 +5,13 @@ path = (r"C:\Users\mahmo\Desktop")
 
 
 ds = os.path.join(path,"online_shoppers_intention.csv")
+df = pd.read_csv(ds)import pandas as pd
+import os
+
+path = (r"C:\Users\mahmo\Desktop")
+
+
+ds = os.path.join(path,"online_shoppers_intention.csv")
 df = pd.read_csv(ds)
 
 # Columns and their respective data types
@@ -98,19 +105,86 @@ print('The recall score is ',recall_score(y_test,y_pred))
 
 
 
+tb=confusion_matrix(y_test,y_pred)
+
+print(tb)
+import matplotlib.pyplot as plt
+
+plt.imshow(tb, cmap='binary')
+
+
 
 "Random Forest Regression"
 from sklearn.ensemble import RandomForestRegressor
-rcmf = RandomForestRegressor(n_estimators=100,max_depth=10)
-rcmf.fit(X_train,y_train)
+#rcmf = RandomForestRegressor(n_estimators=100,max_depth=10)
+#rcmf.fit(X_train,y_train)
 
-x_pred = rclf.predict(X_test)
-print('The accuracy is ',accuracy_score(y_test,x_pred))
-print('The recall score is ',recall_score(y_test,x_pred))
+#x_pred = rclf.predict(X_test)
+#print('The accuracy is ',accuracy_score(y_test,x_pred))
+#print('The recall score is ',recall_score(y_test,x_pred))
 
+
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import r2_score
+
+
+
+def Scoring(y_pred):
+        #The proportion of the variance in the dependent variable predictions 
+        #made on basis of the independent variable(s)
+        #Best possible score is 1.0 
+        print('Test Variance score: %.2f' % r2_score(y_test, y_pred))
+        #arithmetic average of the absolute errors (e = y_pred - y). (closer to zero is better)
+        print('Mean Absolute Error:%.2f'% mean_absolute_error(y_test, y_pred))
+        #the average squared difference between the estimated values and the actual value. (closer to zero is better)
+        print('Mean Squared Error:%.2f'% mean_squared_error(y_test, y_pred))
+        #square root of the average of squared errors. (closer to zero is better)
+        print('\n\n')
+
+
+
+
+def fitTrainScore (dtRegressor):
+    dtRegressor.fit(X_train, y_train)
+    reg_y_pred = dtRegressor.predict(X_test)
+    Scoring(reg_y_pred)
+
+
+dtRegressor =  RandomForestRegressor(random_state=10)
+fitTrainScore(dtRegressor)
 
 
 """Our model succed to predit on test data with 90% accuracy."""
+
+import matplotlib.pyplot as plt
+
+
+plt.figure(figsize=(25,20))
+plt.subplot(5, 3, 1)
+fig = df.boxplot(column='Administrative')
+plt.subplot(5, 3, 2)
+fig = df.boxplot(column='Administrative_Duration')
+plt.subplot(5, 3, 3)
+fig = df.boxplot(column='Informational')
+plt.subplot(5, 3, 4)
+fig = df.boxplot(column='Informational_Duration')
+plt.subplot(5, 3, 5)
+fig = df.boxplot(column='ProductRelated')
+plt.subplot(5, 3, 6)
+fig = df.boxplot(column='ProductRelated_Duration')
+plt.subplot(5, 3, 7)
+fig = df.boxplot(column='BounceRates')
+plt.subplot(5, 3, 8)
+fig = df.boxplot(column='ExitRates')
+plt.subplot(5, 3, 9)
+fig = df.boxplot(column='PageValues')
+plt.subplot(5, 3, 10)
+fig = df.boxplot(column='SpecialDay')
+plt.subplot(5, 3, 11)
+fig = df.boxplot(column='Month')
+plt.subplot(5, 3, 12)
+fig = df.boxplot(column='Region')
 
 
 
